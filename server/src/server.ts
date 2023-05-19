1aa4413c95e597cdda7cab9ce621b28591374013
severinocarlos <juninho_456@hotmail.com>
2023-05-19 12:28:44 -0300

    feat: adding env variables and components

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,19 +1,17 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
-
+import cors from '@fastify/cors'
+import { memoriesRoutes } from './routes/memories'
 
 const app = fastify()
-const prisma = new PrismaClient()
 
-app.get('/users', async () => {
-    const users = await prisma.users.findMany()
-
-    return users
+app.register(cors, {
+    origin: true
 })
+app.register(memoriesRoutes)
 
-
-app.listen({
-    port: 3333
-}).then(() => {
-    console.log('🚀Server runinng on http://localhost:3333')
-})
+app
+    .listen({
+        port: 3333
+    }).then(() => {
+        console.log('🚀Server runinng on http://localhost:3333')
+    })
---
+++ web/src/components/Copyright.tsx
@@ -0,0 +1,15 @@
+export function Copyright() {
+    return (
+        <div className='text-sm leading-relaxed text-gray-200'>
+          Feito com 💜 no NLW da{' '}
+          <a 
+            href="https:rocketseat.com.br"
+            rel="noneferrer"
+            className='underline hover:text-gray-50'
+            target="_blank"
+          >  
+            Rocketseat
+          </a> 
+        </div>
+    )
+}
