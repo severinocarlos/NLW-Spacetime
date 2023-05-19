478a0db531498e82b985cf4a065b5a11b6738b25
severinocarlos <juninho_456@hotmail.com>
2023-05-19 15:33:40 -0300

    feat: adding user authentication in backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,25 @@
+import 'dotenv/config'
+
+import jwt from '@fastify/jwt'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
     origin: true
 })
+
+app.register(jwt, {
+    secret: 'u89quenrv98245btnaehr98g44059hy0t',
+})
+
 app.register(memoriesRoutes)
+app.register(authRoutes)
+
+
 
 app
     .listen({
