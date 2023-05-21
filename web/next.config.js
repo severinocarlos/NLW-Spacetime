7e120358391a47f724f5f1d1963b290a2f1c73c8
severinocarlos <juninho_456@hotmail.com>
2023-05-21 13:56:42 -0300

    feat(memories): list of memories

--- web/next.config.js
+++ web/next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     images: {
         domains: [
-            "avatars.githubusercontent.com"
+            "avatars.githubusercontent.com", '192.168.0.106'
         ]
     }
 }
